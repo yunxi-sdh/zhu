05c56c5a6476110f725c0dafb4443f2c81c55f4a
yunxi <72216451+yunxi-sdh@users.noreply.github.com>
2021-07-13 12:13:24 +0800

    Create 123.php

---
+++ 123.php
@@ -0,0 +1,91 @@
+
+<?php
+include_once 'head.php';
+?>
+<?php $name= file_get_contents("name.php"); ?>
+
+
+<meta name="description" content="<?php echo $name; ?>API,<?php echo $name; ?>API系统">
+<meta name="keywords" content="<?php echo $name; ?>API,<?php echo $name; ?>API系统">
+
+<center><?php echo $name; ?>API404页面<br>
+<br>
+<center><b> <?php echo $name; ?>API未找到该页面哦<br>请确认网址是否正确</b></center><br>
+<img src="img/2.gif" width="254" height="30" <="" body="">
+<marquee><b>     </b></marquee><b><a style="display: block; text-align: center;">
+欢迎来自<script src="//pv.sohu.com/cityjson?ie=utf-8"></script>
+                   <script type="text/javascript">
+                    document.write(returnCitySN["cname"])
+                   </script></di</div></a>
+
+你的IP:<script src="//pv.sohu.com/cityjson?ie=utf-8"></script>
+                   <script type="text/javascript">
+                    document.write(returnCitySN["cip"])
+                   </script></di</div></a>
+<br>
+
+<script language="JavaScript"> 
+<!-- 
+function talk(){ 
+if (document.form1.qqhao.value=="") 
+{ 
+  alert("请输入对方QQ号码！"); 
+  document.form1.qqhao.focus(); 
+  return; 
+} 
+window.location="http://wpa.qq.com/msgrd?V=1&Uin="+document.form1.qqhao.value+"&Site=qqjia.com&Menu=yes"; 
+} 
+--> 
+</script> 
+
+
+
+<form name=form1 action="javascript:talk()"> 
+输入对方QQ号码： 
+<input type=text name=qqhao size=11> 
+<input type=submit value="开始聊天"> 
+</form>
+
+    <br>
+
+
+                    <a href="https://qm.qq.com/cgi-bin/qm/qr?k=vd-vKBu3OTlhUaYZ3MZ0g1KiJ_cpxFdY&noverify=0 target="_blank" rel="nofollow"><font
+
+                                color="#009966">联系客服</font></a>丨
+
+                    <a href="/" target="_blank" rel="nofollow"><font color="#6666cc">返回主页</font></a>
+
+        
+
+              
+<div title="MySSL 安全签章" id="myssl_seal" onclick="window.open('https://myssl.com/seal/detail?domain=api.qfyu.top','MySSL安全签章','height=800,width=470,top=0,right=0,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no')" style="text-align: center"><img src="https://sealres.myssl.com/seal/img/2x/seal.svg?domain=api.qfyu.top" alt="MySSL 安全签章" style="width: 130px; height: auto; cursor: pointer"></div>
+<div title="TrustAsia 安全签章" id="myssl_seal" onclick="window.open('https://seal.trustasia.com/seal/detail?domain=api.qfyu.top','TrustAsia 安全签章','height=800,width=470,top=0,right=0,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no')" style="text-align: center"><img src="https://sealres.trustasia.com/seal/img/2x/seal.svg?domain=api.qfyu.top" alt="TrustAsia 安全签章" style="width: 150px;height: auto; cursor: pointer"></div>
+</tbody></p>
+
+<br>                   
+                                                      
+ <script type="text/javascript">
+var tx=new Array("当前为<?php echo $name; ?>API404页面","请确认网址是否正确","确认正确请点击联系客服","站长QQ:1072398826");
+var txcount=4;
+var i=1;
+var wo=0;
+var ud=1;
+function animatetitle()
+{
+window.document.title=tx[wo].substr(0,i)+"";
+if(ud==0)i--;
+if(ud==1)i++;
+if(i==-1){ud=1;i=0;wo++;wo=wo%txcount;}
+if(i==tx[wo].length+10){ud=0;i=tx[wo].length;}
+parent.window.document.title=tx[wo].substr(0,i)+"";
+setTimeout("animatetitle()",100);
+}
+animatetitle();
+</script>
+</html>                                                                                
+
+
+
+
+
+            
